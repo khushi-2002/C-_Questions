d9653a8c6379b650296014db7f0a941920ca7929
Khushi Agarwal <86999461+khushi-2002@users.noreply.github.com>
2022-07-17 16:49:05 +0530

    Create lcm-hcf.cpp

---
+++ lcm-hcf.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int a,b,r,lcm,hcf;
+  cin>>a>>b;
+  int max= (a>b)?a:b;
+  int min =(a<b)?a:b;
+  
+  while(true){
+    r= max/min;
+    if(r==0){
+     hcf=min;
+      break;
+    }
+    max=min;
+    min=r;
+  }
+   
+  int lcm= (a*b)/hcf;
+  cout<<"HCF is "<<hcf<<endl;
+  cout<<"LCM is "<<lcm<<endl;
+}
